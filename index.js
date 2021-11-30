da125a72e161a21071fc540d4be93de02f1aeac1
Walter Morris <waltercmorris18@gmail.com>
2021-11-30 15:55:44 -0800

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(block) {
+	return Math.abs(42-block);
+}
+
+function distanceFromHqInFeet(block) {
+	return Math.abs(distanceFromHqInBlocks(block))*264;
+}
+
+
+function distanceTravelledInFeet(start,desti) {
+	return Math.abs(start-desti)*264;
+}
+
+function calculatesFarePrice(start,desti) {
+	const distance = distanceTravelledInFeet(start,desti)
+	if (distance < 400) {
+		return 0;
+	}
+	else if (distance >= 400 && distance <= 2000) {
+		return (distance - 400) * .02;
+	}
+	else if (distance > 2000 && distance < 2500) {
+		return 25;
+	}
+	else {
+		return 'cannot travel that far';
+	}
+}
+
